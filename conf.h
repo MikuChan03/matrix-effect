b7984bb9d88b1ae90a99572641f94c9f0e28ddef
MikuChan03 <39016940+MikuChan03@users.noreply.github.com>
2021-01-09 08:49:38 +0100

    Create conf.h

---
+++ conf.h
@@ -0,0 +1,18 @@
+
+#ifndef _whatever
+#define _whatever
+
+/* -----------------start---------------------*/
+/* how big the gap between two following snakes has to be */
+#define SNAKES_MIN_GAP      3
+/* how like it as a for a new snake to start per move in percent */
+#define SNAKES_LIKELIHOOD   3
+/* the minimum length of a snake in percent */
+#define SNAKES_LENGTH_MIN   25
+/* the maximum length of a snake in percent */
+#define SNAKES_LENGTH_MAX   70
+/* speed in ms */
+#define SNAKES_SPEED        45
+/* -----------------stop----------------------*/
+
+#endif
